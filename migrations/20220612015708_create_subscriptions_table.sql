3dbd3dcb1b9f2d5b96448d485687a5c6f8fc0033
Caleb Castleberry <castle.caleb@gmail.com>
2022-06-11 22:43:53 -0400

    added sqlx and db setup

---
+++ migrations/20220612015708_create_subscriptions_table.sql
@@ -0,0 +1,10 @@
+-- migrations/20220612015708_create_subscriptions_table.sql
+-- Create Subscriptions Table
+CREATE TABLE subscriptions
+(
+    id            uuid        NOT NULL,
+    PRIMARY KEY (id),
+    email         TEXT        NOT NULL UNIQUE,
+    name          TEXT        NOT NULL,
+    subscribed_at timestamptz NOT NULL
+);
